b5210e468143c5d50845bdc69fb61f135d92f9a9
clement880101 <clement880101@gmail.com>
2021-03-02 02:52:59 -0500

    Support multiple query

--- searchterm.py
+++ searchterm.py
@@ -1,38 +1,49 @@
 import pandas as pd
-from datetime import datetime
+import datetime as dt
 import praw
 
 
 def main():
-    search_term = "gme"
-    category = "all"
+    search_term = ["gme", "amc", "tlry", "acb"]
+    category = ["stock", "stock", "stock", "stock"]
+    period = 100
 
     client_id = 'lql9yxsiL66vew'
     client_secret = ''
     user_agent = 'redditscrape'
 
     reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+    targetdate = dt.datetime.utcnow().date()
+    data = pd.DataFrame(pd.date_range(end=targetdate, periods=period), columns=["date"])
+    data = data.iloc[::-1].reset_index(drop=True)
 
-    # Instead of /all, this search is more comprehensive
-    list = reddit.subreddits.search(category)
+    if len(category) != len(search_term):
+        return -1
 
-    maxutc = (datetime.utcnow().timestamp() // 86400) * 86401
-    data = pd.Series(0, index=pd.date_range(end=datetime.today().strftime('%Y-%m-%d'), periods=100),
-                     name="occurrences")
-    data = data.iloc[::-1]
+    for z in range(len(search_term)):
+        # Instead of /all, this search is more comprehensive
+        print("Category: " + category[z])
+        print("Search Term: " + search_term[z])
+        print("*********")
+        subreddit = reddit.subreddits.search(category[z])
 
-    for subred in list:
-        print("Subreddit: ", subred.display_name)
-        feed = subred.search(search_term, sort="new", time_filter="year")
-        for post in feed:
-            for i in range(len(data)):
-                if (post.created_utc <= maxutc - (i * 86400)) and (post.created_utc > maxutc - ((i + 1) * 86400)):
-                    data[i] = data[i] + 1
-                    break
-        print("complete")
+        temp = [0] * period
+        for subred in subreddit:
+            print("Subreddit: ", subred.display_name)
+            feed = subred.search(search_term[z], sort="new", time_filter="year")
+            for post in feed:
+                postdate = dt.datetime.fromtimestamp(post.created_utc).date()
+                for i in range(len(temp)):
+                    pp = targetdate - dt.timedelta(i)
+                    if postdate == pp:
+                        temp[i] = temp[i] + 1
+                        break
+        temp = pd.Series(temp, name=search_term[z])
+        data = pd.concat((data, temp), axis=1)
+        print("-----------------")
 
-    name = search_term + "," + category + ".csv"
-    data.to_csv(name, index=True)
+    name = search_term[0] + "-" + category[0] + ".csv"
+    data.to_csv(name, index=False)
 
 
 if __name__ == "__main__":
